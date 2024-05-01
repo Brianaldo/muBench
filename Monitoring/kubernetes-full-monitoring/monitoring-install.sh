0153da7054fee26833c6bb6aea810e6b95ae105a
Brianaldo <brianaldophandiarta31@gmail.com>
2024-05-01 10:36:01 +0700

    feat: modify monitoring installation

--- Monitoring/kubernetes-full-monitoring/monitoring-install.sh
+++ Monitoring/kubernetes-full-monitoring/monitoring-install.sh
@@ -10,30 +10,33 @@ helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring
 # Prometheus (30000) and Grafana (30001) NodePort Services
 kubectl apply -f prometheus-nodeport.yaml -n monitoring
 kubectl apply -f grafana-nodeport.yaml -n monitoring
+kubectl apply -f ./mub-monitor.yaml
+kubectl apply -f ./prometheus-nodeport.yaml -n monitoring
+kubectl apply -f ./grafana-nodeport.yaml -n monitoring
 
-# Istio
-helm repo add istio https://istio-release.storage.googleapis.com/charts
-helm repo update
-kubectl create namespace istio-system
-helm install istio-base istio/base -n istio-system
-helm install istiod istio/istiod -n istio-system --wait
-kubectl create namespace istio-ingress
-kubectl label namespace istio-ingress istio-injection=enabled
-kubectl label namespace default istio-injection=enabled
-helm install istio-ingress istio/gateway -n istio-ingress
+# # Istio
+# helm repo add istio https://istio-release.storage.googleapis.com/charts
+# helm repo update
+# kubectl create namespace istio-system
+# helm install istio-base istio/base -n istio-system
+# helm install istiod istio/istiod -n istio-system --wait
+# kubectl create namespace istio-ingress
+# kubectl label namespace istio-ingress istio-injection=enabled
+# kubectl label namespace default istio-injection=enabled
+# helm install istio-ingress istio/gateway -n istio-ingress
 
-# Istio - Prometeus integration
-kubectl apply -f istio-prometheus-operator.yaml
+# # Istio - Prometeus integration
+# kubectl apply -f istio-prometheus-operator.yaml
 
-# Jarger
-kubectl apply -f jaeger.yaml
+# # Jarger
+# kubectl apply -f jaeger.yaml
 
-# Jaeger NodePort Service (30002)
-kubectl apply -f jaeger-nodeport.yaml
+# # Jaeger NodePort Service (30002)
+# kubectl apply -f jaeger-nodeport.yaml
 
-#Kiali
-kubectl apply -f kiali.yaml
+# #Kiali
+# kubectl apply -f kiali.yaml
 
-#Kiali NodePort Service (30003)
-kubectl apply -f kiali-nodeport.yaml
+# #Kiali NodePort Service (30003)
+# kubectl apply -f kiali-nodeport.yaml
 
